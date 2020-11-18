97de5c1d1827d3018c87cdeb60491f6a5b900f07
s-person27 <69790023+s-person27@users.noreply.github.com>
2020-11-18 15:13:29 +0200

    Delete view.py

--- DB_lab2/view.py
+++
@@ -1,7 +0,0 @@
-def Menu():
-    print("If you want to update press 1")
-    print("If you want to add press 2")
-    print("If you want to delete press 3")
-    print("If you want to random press 4")
-    print("If you want to search press 5")
-
